d4ece690594585e2c599fb2561b7d88c09a3d7c3
David Cutura <davidcutura@Davids-MacBook-Pro.local>
2024-07-16 13:49:51 +0200

    added custom GUI and connected to Filters

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,15 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider
+{
+    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                        juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+        
+    }
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +37,28 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     EQoonAudioProcessor& audioProcessor;
-
+    
+    CustomRotarySlider peakFreqSlider,
+    peakGainSlider,
+    peakQualitySlider,
+    lowCutFreqSlider,
+    highCutFreqSlider,
+    lowCutSlopeSlider,
+    highCutSlopeSlider;
+    
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachement = APVTS::SliderAttachment;
+    
+    Attachement peakFreqSliderAttachement,
+                peakGainSliderAttachement,
+                peakQualitySliderAttachement,
+                lowCutFreqSliderAttachement,
+                highCutFreqSliderAttachement,
+                lowCutSlopeSliderAttachement,
+                highCutSlopeSliderAttachement;
+     
+    
+    std::vector<juce::Component*> getComps();
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EQoonAudioProcessorEditor)
 };
